bbb0659c8cd8658df598b28dc0c251e06125d7df
senyum-menawan <jessiefilia7@gmail.com>
2022-10-09 19:13:13 +0800

    tommy

---
+++ spec/tommytom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TommytomView from '../lib/tommytom-view';
+
+describe('TommytomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
